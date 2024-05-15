ea34c41ed2b79dfed4dba6d91a1ff71e6af30c04
Montero Charm Venice <migz.cresencio@gmail.com>
2024-05-16 00:29:43 +0800

    SET UP

---
+++ PREFINALS_AUTH_CRESENCIO_RODOLFO/PREFINALS_AUTH_CRESENCIO_RODOLFO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_CRESENCIO_RODOLFO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
